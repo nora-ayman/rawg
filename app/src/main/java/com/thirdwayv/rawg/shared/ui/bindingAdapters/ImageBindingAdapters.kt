ccec5a1981cb454169a3f007bcae8e7e406f1078
Nora Eleish <nora.a.eleish@gmail.com>
2021-11-24 23:03:22 +0200

    ui adjustments

--- app/src/main/java/com/thirdwayv/rawg/shared/ui/bindingAdapters/ImageBindingAdapters.kt
+++ app/src/main/java/com/thirdwayv/rawg/shared/ui/bindingAdapters/ImageBindingAdapters.kt
@@ -28,6 +28,17 @@ fun setImageWithTopRoundedCorners(view: ImageView, url: String?) {
 
 }
 
+@BindingAdapter("imageUrlStartRoundedCorners")
+fun setImageWithStartRoundedCorners(view: ImageView, url: String?) {
+    Glide
+        .with(view.context)
+        .load(url)
+        .fitCenter()
+        .transform(GranularRoundedCorners(18f, 0f, 0f, 18f))
+        .into(view)
+
+}
+
 @BindingAdapter("imageUrl")
 fun setImage(view: ImageView, url: String?) {
     Glide
